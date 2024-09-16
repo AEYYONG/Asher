b08616a449c6d3f81b4e43d968c8f5c92df015a4
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2024-09-16 16:57:29 +0900

    Comment : 맵 생성 커스텀 에디터 주석 추가

--- Assets/Editor/MapGenerator.cs
+++ Assets/Editor/MapGenerator.cs
@@ -10,15 +10,15 @@ using UnityEngine.SceneManagement;
 public class MapGenerator : EditorWindow
 {
     //Grid 생성
-    private int _gridWidth;
-    private int _gridHeight;
-    private GameObject _gridPrefab;
-    private GameObject _gridParent;
-    private List<GameObject> _gridList = new List<GameObject>();
+    private int _gridWidth; //grid 가로 개수
+    private int _gridHeight; //grid 세로 개수 
+    private GameObject _gridPrefab; //grid prefab
+    private GameObject _gridParent; //생성된 grid들이 들어갈 부모 오브젝트
+    private List<GameObject> _gridList = new List<GameObject>(); //grid 오브젝트들을 저장할 리스트
     
     //타일 생성
-    private int _prevWidth;
-    private int _prevHeight;
+    private int _prevWidth; //이전 타일 너비
+    private int _prevHeight; //이전 타일 높이
     private int _curWidth; //타일 너비
     private int _curHeight; //타일 높이
     private GameObject _tilePrefab; //타일 프리팹
@@ -68,6 +68,8 @@ public class MapGenerator : EditorWindow
 
     private void OnDisable()
     {
+        //커스텀 에디터 창이 끌 때
+        //타일 매니저에 해당 타일의 정보 저장
         _tileManager.width = _curWidth;
         _tileManager.height = _curHeight;
         EditorUtility.SetDirty(_tileManager);
@@ -76,19 +78,22 @@ public class MapGenerator : EditorWindow
 
     private void OnGUI()
     {
+        //grid 생성 부분
         GUILayout.Label("Generate Grid",EditorStyles.largeLabel);
         _gridWidth = EditorGUILayout.IntField("grid width", _gridWidth);
         _gridHeight = EditorGUILayout.IntField("grid height", _gridHeight);
+        //generate 버튼 클릭 시, grid 생성 함수 호출
         if (GUILayout.Button("Generate Grid"))
         {
             GenerateGrid(_gridWidth,_gridHeight);
         }
-
+        //destroy 버튼 클릭 시, grid 삭제 함수 호출
         if (GUILayout.Button("Destroy Grid"))
         {
             DestroyGrid();
         }
         EditorGUILayout.Space(10);
+        //타일 생성 부분
         GUILayout.Label("Generate Tile",EditorStyles.largeLabel);
         _curWidth = EditorGUILayout.IntSlider("width",_curWidth,0,20);
         _curHeight = EditorGUILayout.IntSlider("height",_curHeight,0,20);
@@ -96,6 +101,7 @@ public class MapGenerator : EditorWindow
         _tileParent = (GameObject)EditorGUILayout.ObjectField("tile parent", _tileParent, typeof(GameObject), true);
         EditorGUILayout.Space();
         
+        //타일의 이전 너비/높이와 현재 너비/높이가 달라졌을 경우 타일 생성/삭제 함수 호출
         if (_prevWidth != _curWidth)
         {
             //가로 값이 달라졌다면
@@ -113,12 +119,14 @@ public class MapGenerator : EditorWindow
     //Grid Map 생성하기
     void GenerateGrid(int w, int h)
     {
+        //grid parent로 사용할 빈 오브젝트 생성하고, 이름을 Grid Parent로 명명하기
         _gridParent = new GameObject();
         _gridParent.name = "Grid Parent";
         for (int i = 0; i < h; i++)
         {
             for (int j = 0; j < w; j++)
             {
+                //grid를 gird parent의 자식으로 생성한 후, grid 리스트에 넣기
                 GameObject grid = PrefabUtility.InstantiatePrefab(_gridPrefab, _gridParent.transform) as GameObject;
                 grid.transform.position = new Vector3(j, -0.1f, i);
                 _gridList.Add(grid);
@@ -128,6 +136,7 @@ public class MapGenerator : EditorWindow
     //Grid Map 삭제하기
     void DestroyGrid()
     {
+        //씬에서 Grid Parent 이름의 오브젝트를 찾기
         GameObject gridParent = GameObject.Find("Grid Parent");
         if (gridParent == null)
         {
@@ -135,21 +144,30 @@ public class MapGenerator : EditorWindow
         }
         else
         {
-            DestroyImmediate(gridParent);   
+            //Grid Parent를 삭제하며 리스트 초기화
+            DestroyImmediate(gridParent);
+            _gridList.Clear();
         }
     }
+    
+    //타일 증가 및 감소 관리 함수
     void GenerateTile()
     {
+        //현재 타일 너비가 직전 타일 너비보다 크다면 -> 타일을 증가하겠다는 의도
         if (_curWidth > _prevWidth)
         {
             //타일이 가로로 증가
+            //현재 높이만큼 채우기
             for (int i = 0; i < _curHeight; i++)
             {
+                //이미 채워져있던 너비 이후 만큼 채우기
                 for (int j = 0; j < _curWidth - _prevWidth; j++)
                 {
+                    //현재 너비 - 이전 너비를 하여 추가되어야 하는 가로 타일 개수(j)를 계산
                     int x = _prevWidth + j;
                     int z = i;
                     Vector2Int pos = new Vector2Int(x, z);
+                    //TileEntry 생성 후, 타일 매니저 내의 타일 리스트에 추가하기
                     TileEntry entry = new TileEntry();
                     entry.position = pos;
                     entry.tile = PrefabUtility.InstantiatePrefab(_tilePrefab,_tileParent.transform) as GameObject;
@@ -159,8 +177,10 @@ public class MapGenerator : EditorWindow
                 }
             }
         }
-        else if(_curWidth < _prevWidth)
+        else if(_curWidth < _prevWidth) //현재 너비가 이전 너비보다 작다면 -> 타일을 가로로 감소하려는 의도
         {
+            //삭제할 타일 엔트리 리스트를 생성, foreach 내에서 리스트 삭제 시 예외처리 발생하기 때문임.
+            //컬렉션을 순회하면서 동시에 수정하려고 하면 런타임 예외가 발생함.
             List<TileEntry> removeEntries = new List<TileEntry>();
             //타일이 가로로 감소
             for (int i = 0; i <_curHeight; i++)
@@ -180,12 +200,15 @@ public class MapGenerator : EditorWindow
                     }
                 }
             }
-
+            //리스트 내에서 타일 엔트리 삭제
             foreach (var entry in removeEntries)
             {
                 _tileManager.tileEntries.Remove(entry);
             }
+            
         }
+        
+        //현재 높이가 이전 높이보다 크다면 -> 높이를 증가하려는 의도
         if (_curHeight > _prevHeight)
         {
             //타일이 세로로 증가
@@ -205,7 +228,7 @@ public class MapGenerator : EditorWindow
                 }
             }
         }
-        else if(_curHeight < _prevHeight)
+        else if(_curHeight < _prevHeight) //현재 높이가 이전 높이보다 작다면 -> 높이를 감소하려는 의도
         {
             List<TileEntry> removeEntries = new List<TileEntry>();
             //타일이 세로로 감소
