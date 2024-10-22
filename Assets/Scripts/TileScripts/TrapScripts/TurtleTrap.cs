d829743b985541fb9b613bb945fc07a32129ca52
KyoungMin Yoon <96673951+yoongang02@users.noreply.github.com>
2024-10-22 15:58:52 +0900

    Feat : 거북이 함정 구현
    거북이 함정 효과 구현

--- Assets/Scripts/TileScripts/TrapScripts/TurtleTrap.cs
+++ Assets/Scripts/TileScripts/TrapScripts/TurtleTrap.cs
@@ -7,6 +7,19 @@ public class TurtleTrap : Tile
     public override void TrapUse(StageUIManager uiManager)
     {
         base.TrapUse(uiManager);
-        Debug.Log("거북이 아이템 사용");
+        Debug.Log("거북이 함정 발동");
+        StartCoroutine(SpeedDown(uiManager));
     }
+
+
+    IEnumerator SpeedDown(StageUIManager uiManager)
+    {
+        Player_Move_anim playerMove = uiManager.player.GetComponent<Player_Move_anim>();
+        playerMove.moveDuration *= tileSO.power;
+
+        yield return new WaitForSeconds(tileSO.duration);
+        Debug.Log("거북이 함정 지속 시간 끝");
+        playerMove.moveDuration *= 1/tileSO.power;
+    }
+    
 }
