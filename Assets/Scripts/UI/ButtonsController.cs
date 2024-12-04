ec9b3120161ded363f3126ca7b19346b49e9b5c0
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2024-12-05 00:01:58 +0900

    Feat: Play 버튼

---
+++ Assets/Scripts/UI/ButtonsController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class ButtonsController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float moveDistance = 250f; // 이동 거리
+    public float duration = 1f; // 애니메이션 지속 시간
+    public Ease easingType = Ease.OutBounce;
+
+    private GameObject[] buttonObjects;
+
+
+    private Vector3 originalScale;// 원래 크기
+
+    public GameObject Windows;
+
+    private bool isMoving = false;
+
+    void Start()
+    {
+
+        // 현제 오브젝트
+        GameObject currentObject = this.gameObject;
+
+        // 다른 버튼 오브젝트
+        buttonObjects = GameObject.FindGameObjectsWithTag("Button");
+
+
+        originalScale = transform.localScale;
+
+    }
+
+    // 마우스가 버튼 위로 올라갔을 때
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(1.1f, 0.1f).SetEase(Ease.OutBack); // 부드럽게 커짐
+    }
+
+    // 마우스가 버튼에서 벗어났을 때
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(1f, 0.1f).SetEase(Ease.InBack); // 원래 크기로 복귀
+    }
+
+    public void OnButtonClick()
+    {
+        isMoving = false;
+
+        Windows.SetActive(true);
+
+        foreach (GameObject button in buttonObjects)
+        {
+            button.transform.DOMoveX(button.transform.position.x - moveDistance, duration)
+                            .SetEase(easingType) // Tween 애니메이션
+                             .OnComplete(() => isMoving = false);
+        }
+    }
+
+    public void ComeBack()
+    {
+        if (isMoving) return;
+
+        isMoving = true;
+
+        foreach (GameObject button in buttonObjects)
+        {
+            button.transform.DOMoveX(button.transform.position.x + moveDistance, duration)
+                            .SetEase(easingType)
+                            .OnComplete(() => isMoving = false);
+        }
+
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("StageNoaP");
+    }
+
+
+
+}
---
+++ Assets/Scripts/UI/OnclickOutside.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnclickOutside : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool isPointerInside = false;
+
+    public GameObject Buttons;
+    private ButtonsController buttonController;
+
+
+    private void Start()
+    {
+        buttonController = Buttons.GetComponent<ButtonsController>();
+    }
+    void Update()
+    {
+        // 마우스 클릭이 감지되었고 포인터가 버튼 밖에 있는 경우
+        if (Input.GetMouseButtonDown(0) && !isPointerInside)
+        {
+            gameObject.SetActive(false);
+
+            buttonController.ComeBack();
+        }
+    }
+
+    public void  OnClickX()
+    {
+        gameObject.SetActive(false);
+
+        buttonController.ComeBack();
+    }
+
+    // 마우스가 버튼 위에 올라갔을 때 호출
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isPointerInside = true;
+    }
+
+    // 마우스가 버튼에서 벗어났을 때 호출
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerInside = false;
+    }
+
+    private bool IsPointerOverUIElement()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // 현재 UI 요소에 포인터가 있는지 확인
+        foreach (var result in results)
+        {
+            if (result.gameObject == gameObject)
+                return true; // 현재 GameObject에 포인터가 있음
+        }
+        return false; // 버튼 밖임
+    }
+}
