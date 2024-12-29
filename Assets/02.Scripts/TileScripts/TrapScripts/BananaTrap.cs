fe99d4d8cd78c10326a74bffed2d34c2abf21495
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2024-12-30 07:56:08 +0900

    Feat : 바나나 함정 - 플레이어 스턴 VFX 구현

--- Assets/02.Scripts/TileScripts/TrapScripts/BananaTrap.cs
+++ Assets/02.Scripts/TileScripts/TrapScripts/BananaTrap.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BananaTrap : Tile
 {
     public GameObject bananaVFX;
+    public GameObject stunVFX;
     public override void TrapUse(StageUIManager uiManager)
     {
         base.TrapUse(uiManager);
@@ -18,6 +19,9 @@ public class BananaTrap : Tile
         Vector3 pos = new Vector3(transform.position.x, 0.3f, transform.position.z);
         GameObject banana = Instantiate(bananaVFX);
         banana.transform.position = pos;
+        
+        //플레이어의 자식 오브젝트로 stun vfx 생성하기
+        GameObject stun = Instantiate(stunVFX,uiManager.player.transform);
         
         VFXManager.Instance.PlayVFX("UseDebuffItem",uiManager.player.transform);
         yield return new WaitForSeconds(1.5f);
@@ -33,6 +37,8 @@ public class BananaTrap : Tile
         
         //바나나 vfx 제거
         Destroy(banana);
+        //stun vfx 제거
+        Destroy(stun);
         //vfx 실행
         Animator effectAnimator = transform.GetChild(0).GetComponent<Animator>();
         effectAnimator.SetTrigger("TrapMatch");
