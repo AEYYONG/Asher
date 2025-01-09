9e29fc66244eb9d8fceced4c820e145b60cf2043
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2025-01-09 09:44:33 +0900

    Fix: 헤어볼(1)

--- Assets/02.Scripts/Player/HairBallVignette.cs
+++ Assets/02.Scripts/Player/HairBallVignette.cs
@@ -5,31 +5,36 @@ using UnityEngine;
 public class HairBallVignette : MonoBehaviour
 {
     private Player_Move player;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         player = FindObjectOfType<Player_Move>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     void Update()
     {
          if (player.useBall)
          {
+            spriteRenderer.enabled = true;
             Vector3 newPosition = player.transform.position;
             newPosition.z += 0.5f;
             transform.position = newPosition;
-            if (!gameObject.activeSelf)
+           /* if (!gameObject.activeSelf)
              {
                  gameObject.SetActive(true);
-             }
+             }*/
          }
 
         else
         {
-            if (gameObject.activeSelf)
-            {
-                gameObject.SetActive(false);
-            }
+            spriteRenderer.enabled = false;
+            /*  if (gameObject.activeSelf)
+              {
+                  gameObject.SetActive(false);
+              }*/
         }
 
     }
