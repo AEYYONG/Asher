f13255fbad2070759753cad2c7413c269ccdfe2b
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2025-02-11 00:27:25 +0900

    Feat : 클리어 대화 씬 (1)

--- Assets/02.Scripts/Dialogue/DialogueManager.cs
+++ Assets/02.Scripts/Dialogue/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,17 +26,28 @@ public class DialogueManager : MonoBehaviour
     [Header("UI Elements")] public GameObject dialougeUI;
     public Image portraitImage; // 초상화 이미지
     public TextMeshProUGUI dialogueText; // 대화 텍스트
+    public TextMeshProUGUI choice1Text; // 선택자1 텍스트
+    public TextMeshProUGUI choice2Text; // 선택지2 텍스트
     public GameObject toggleButton; // 토글 버튼
 
-    [Header("Dialogue Data")]
-    public List<DialogueData> dialogues; // 대화 데이터 목록
+    [Header("Dialogue Data")] [SerializeField]
+    private string _startDialougeKey;
+    private Dictionary<string, DialogueData> _dialogueDatas = new Dictionary<string, DialogueData>();
 
     [Header("Typing Settings")]
     public float typingSpeed = 0.05f; // 한 글자 출력 속도
+    private Coroutine _typingCoroutine;
+    private bool _isTyping = false; // 현재 텍스트가 출력 중인지 여부
+    public Action OnSelectEnd;
 
-    private int currentDialogueIndex = 0; // 현재 대화 인덱스
-    private Coroutine typingCoroutine;
-    private bool isTyping = false; // 현재 텍스트가 출력 중인지 여부
+    [Header("Portrait Data")] [SerializeField]
+    private List<Sprite> _portraits;
+
+    private void Awake()
+    {
+        ParserCSV parser = new ParserCSV();
+        _dialogueDatas = parser.ParseDialogue(_startDialougeKey);
+    }
 
     private void Start()
     {
@@ -46,9 +58,9 @@ public class DialogueManager : MonoBehaviour
     private void Update()
     {
         // SPACE 키 입력 처리
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && OnSelectEnd == null)
         {
-            if (isTyping) // 현재 대사가 출력 중이라면
+            if (_isTyping) // 현재 대사가 출력 중이라면
             {
                 // 모든 문장을 즉시 출력
                 FinishTyping();
@@ -62,38 +74,62 @@ public class DialogueManager : MonoBehaviour
     }
     public void StartDialogue()
     {
-        currentDialogueIndex = 0;
-        ShowDialogue(currentDialogueIndex);
+        ShowDialogue(_startDialougeKey);
     }
     
-    private void ShowDialogue(int index)
+    private void ShowDialogue(string key)
     {
-        if (index >= dialogues.Count)
-        {
-            Debug.Log("모든 대화를 완료했습니다.");
-            EndDialogue();
-            return;
-        }
-
-        // UI 초기화
-        toggleButton.SetActive(false);
-        dialogueText.text = "";
-
-        // 데이터 적용
-        DialogueData dialogueData = dialogues[index];
-        //portraitImage.sprite = dialogueData.portrait;
-
-        // 대사 출력 시작
-        if (typingCoroutine != null)
-        {
-            StopCoroutine(typingCoroutine);
-        }
-        //typingCoroutine = StartCoroutine(TypeDialogue(dialogueData.dialogueText));
+        //키 값에 따른 데화 데이터 가져오기
+        //키 내의 텍스트 데이터 모두 출력하기
+        //선택지가 있는 텍스트면 
+        
+        
+        // // 데이터 적용
+        // DialogueData dialogueData = _dialogueDatas[key];
+        // // 초상화 설정
+        // if (dialogueData.characterId == "Character_01")
+        // {
+        //     portraitImage.sprite = _portraits[0];
+        // }
+        // else
+        // {
+        //     portraitImage.sprite = _portraits[1];
+        // }
+        //
+        // foreach (var textData in dialogueData.textDatas)
+        // {
+        //     // UI 초기화
+        //     toggleButton.SetActive(false);
+        //     dialogueText.text = "";
+        //     OnSelectEnd = null;
+        //     //선택지
+        //     if (!string.IsNullOrEmpty(textData.choiceText1) && !string.IsNullOrEmpty(textData.choiceText2))
+        //     {
+        //         choice1Text.text = textData.choiceText1;
+        //         choice2Text.text = textData.choiceText2;
+        //         OnSelectEnd += () =>
+        //         {
+        //             ShowDialogue(textData.nextDialougeId);
+        //         };
+        //     }
+        //     else
+        //     {
+        //         choice1Text.text = "";
+        //         choice2Text.text = "";
+        //     }
+        // }
+        //
+        // // 대사 출력 시작
+        // if (_typingCoroutine != null)
+        // {
+        //     StopCoroutine(_typingCoroutine);
+        // }
+        // _typingCoroutine = StartCoroutine(TypeDialogue(dialogueData.dialogueText));
     }
     
     private IEnumerator TypeDialogue(string text)
     {
-        isTyping = true;
+        _isTyping = true;
         dialogueText.text = "";
 
         foreach (char letter in text.ToCharArray())
@@ -108,33 +144,33 @@ public class DialogueManager : MonoBehaviour
 
     private void FinishTyping()
     {
-        if (typingCoroutine != null)
-        {
-            StopCoroutine(typingCoroutine);
-        }
-
-        DialogueData dialogueData = dialogues[currentDialogueIndex];
-        //dialogueText.text = dialogueData.dialogueText;
-
-        isTyping = false;
-        toggleButton.SetActive(true);
+        // if (typingCoroutine != null)
+        // {
+        //     StopCoroutine(typingCoroutine);
+        // }
+        //
+        // DialogueData dialogueData = dialogues[currentDialogueIndex];
+        // //dialogueText.text = dialogueData.dialogueText;
+        //
+        // isTyping = false;
+        // toggleButton.SetActive(true);
     }
     
     private void NextDialogue()
     {
-        currentDialogueIndex++;
-        if (currentDialogueIndex == 1)
-        {
-            GetComponent<Animator>().SetTrigger("Transition");
-        }
-        if (currentDialogueIndex < dialogues.Count)
-        {
-            ShowDialogue(currentDialogueIndex);
-        }
-        else
-        {
-            EndDialogue();
-        }
+        // currentDialogueIndex++;
+        // if (currentDialogueIndex == 1)
+        // {
+        //     GetComponent<Animator>().SetTrigger("Transition");
+        // }
+        // if (currentDialogueIndex < dialogues.Count)
+        // {
+        //     ShowDialogue(currentDialogueIndex);
+        // }
+        // else
+        // {
+        //     EndDialogue();
+        // }
     }
     
     private void EndDialogue()
